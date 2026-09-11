29a13508d06d6e44f8b86962e6aad3b7a9ceda67
kyobashi0gg-droid <kyobashi0.gg@gmail.com>
2026-09-11 19:19:12 +0900

    Refresh offline cache for v3 importer

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'my-keiba-lab-v2';
-const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest'];
+const CACHE = 'my-keiba-lab-v3';
+const ASSETS = ['./', './index.html', './styles.css', './app.js', './premium-v3.js', './manifest.webmanifest'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
