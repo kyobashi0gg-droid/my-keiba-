f267e6ff0125e17f478d49e5886e4d1b5eefd74c
kyobashi0gg-droid <kyobashi0.gg@gmail.com>
2026-09-13 19:33:28 +0900

    Load v22 DB score and consult integration

--- editorial-ui-v14-fix.js
+++ editorial-ui-v14-fix.js
@@ -37,7 +37,7 @@
 (() => {
   if (window.__MYKEIBA_POST_STABILITY_LOADER__) return;
   window.__MYKEIBA_POST_STABILITY_LOADER__ = true;
-  for (const src of ['./race-ui-v17.js', './horse-db-v18.js', './horse-db-name-fix-v19.js', './horse-db-summary-v20.js', './horse-db-fit-v21.js']) {
+  for (const src of ['./race-ui-v17.js', './horse-db-v18.js', './horse-db-name-fix-v19.js', './horse-db-summary-v20.js', './horse-db-fit-v21.js', './horse-db-score-consult-v22.js']) {
     const script = document.createElement('script');
     script.src = src;
     script.defer = true;
